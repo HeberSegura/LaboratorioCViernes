8ebddc2f3b67b40cb3f846f4048e66ce398b74c6
HeberSegura <heber.segura.gonzalez@gmail.com>
2019-08-23 08:56:32 -0500

    Primer commit Practica1

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
+int main(void) {
+	
+	int num=1;
+	printf("El numero es %d",num);
+	
 	return 0;
 }
