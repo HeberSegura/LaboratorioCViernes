2703dc7392bb91386944a261943deddd8e84b62e
HeberSegura <54339915+HeberSegura@users.noreply.github.com>
2019-08-24 18:33:17 -0500

    Actividad 1 (problemas 1 y 2)

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+/*
+*Autor: Heber Segura Gonzalez
+*Fecha: 24/08/2019
+*Grupo: 001
+*Horario: Viernes 7am-9am
+*Matricula: 187736
+*/
 
-int main(void) {
-	
-	int num=1;
-	printf("El numero es %d",num);
+int main(void){
+
+	float x; //Variable para el valor de x en la operacion (tiene que ser un float para que las operaciones funcionen)
+	float op1, op2; //Variables para calcular el valor de la operacion
+	printf("Introduzca un valor para x.\n");
+	scanf("%f",&x);
+
+	//Calculando ambas operaciones
+	op1 = ((x-1)/4)-((x-5)/36); 
+	op2 = 6*((x+1)/8-((2*x-3)/16));
+
+	//Escribiendo en la consola los resultados de las operaciones
+	printf("\nEl valor de la operacion ((x-1)/4)-((x-5)/36), donde x es igual a %.2f, es %.4f.\n\nEl valor de la operacion 6*((x+1)/8-((2*x-3)/16)), donde x es igual a %.2f, es %.4f.",x,op1,x,op2);
 	
 	return 0;
 }
