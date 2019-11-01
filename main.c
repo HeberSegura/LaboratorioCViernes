dcb1b04ec2590dfc6377ec90fe11043e72bb0f9d
HeberSegura <heber.segura.gonzalez@gmail.com>
2019-11-01 08:36:11 -0600

    Pracitca13

--- main.c
+++ main.c
@@ -1,8 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+*Autor: Heber Segura Gonzalez
+*Fecha: 1/11/2019
+*Grupo: 006
+*Horario: Viernes 7am-9am
+*Matricula: 1877364
+*/
 
-int main(int argc, char *argv[]) {
+int main(void) {
+	
+	/*
+	************ Actividad 1 ************
+	*/
+	printf("Actividad 1\n");
+	
+	//Decalrando cadena y obteniendo un valor
+	char str[50];
+	printf("Introduzca una cadena de caracteres: ");
+	gets(str);
+	
+	//Declarando puntero e inicializando en inicio de str
+	char *c = &str;
+	
+	//Declarando iterador y contador de vocales
+	int i=0,vowCount=0;
+	
+	//Ciclo para contar las vocales
+	for(i=0;i<strlen(str);i++){
+		//El switch checa la posicion de memoria c+i y convierte el caracter en esa posicion a minuscula. Si es vocal, suma 1 a vowCount
+		switch(tolower(*(c+i))){
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				vowCount++;
+				break;
+			default:
+				break;
+		}	
+	}
+	
+	//Imprimiendo el resultado
+	printf("\nHay %d vocales en \"%s\".",vowCount,str);
+	
+	/*
+	************ Actividad 2 ************
+	*/
+	printf("\n\n***************************\nActividad 2\n");
+	
+	int num1,num2;
+	printf("Introduzca el primer numero entero: ");
+	scanf("%d",&num1);
+	printf("Introduzca el segundo numero entero: ");
+	scanf("%d",&num2);
+	
+	int *n1=&num1,*n2=&num2;
+	
+	printf("\n%d + %d = %d",*n1,*n2,(*n1+*n2));
+	
 	return 0;
 }
